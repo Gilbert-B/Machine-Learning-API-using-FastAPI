ec0a505e6939783f91c0deb34fbea0905a6d4ddb
Gilbert Botchway <botchwaykojo.@gmail.com>
2023-05-30 16:00:26 +0000

    completed API

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 import logging
 import uvicorn
 import os
+from uvicorn import run
+
 
 """ Next, we configure a logger, that helps us keep track of what our program is doing.
 Like a  notebook where we write down what our program is doing at different points."""
@@ -35,8 +37,9 @@ scaler = load_scaler()
 
 """We define a function that will make predictions using our model and scaler."""
 def predict(df, endpoint="simple"):
-    
-    return parsed_output
+    scaled_data = scaler.transform(df)
+    predictions = model.predict(scaled_data)
+    return predictions
 
 """We create models for the data that our API will work with. 
 We define what kind of information the data will have.
@@ -63,3 +66,53 @@ class Patients(BaseModel):
     @classmethod
     def return_list_of_dict(cls, patients: "Patients"):
         return [patient.dict() for patient in patients.all]
+
+
+"""Next block of code defines different parts of our API and how it responds to different requests. 
+It sets up a main page with a specific message, provides a checkup endpoint to receive 
+optional parameters, and sets up prediction endpoints to receive medical data for making predictions,
+either for a single patient or multiple patients."""
+
+@app.get("/")
+def root():
+    logger.info("Root endpoint called.")
+    return {"API": "This is an API for Sepssis prediction."}
+
+@app.get("/checkup")
+def checkup(a: int = None, b: int = 0):
+    logger.info(f"Checkup endpoint called with parameters: a={a}, b={b}.")
+    return {"a": a, "b": b}
+
+@app.post("/predict")
+def predict_sepsis(patient: Patient):
+    logger.info("Predict endpoint called.")
+    # Convert the patient data to a pandas DataFrame
+    df = pd.DataFrame([patient.dict()])
+
+    # Preprocess the data using your scaler
+    scaled_data = scaler.transform(df)
+
+    # Make predictions using your model
+    predictions = model.predict(scaled_data)
+
+    # Return the predictions
+    return {"predictions": predictions.tolist()}
+
+@app.post("/predict_multi")
+def predict_sepsis_for_multiple_patients(patients: Patients):
+    logger.info("Predict Multi endpoint called.")
+     # Convert the patients' data to a pandas DataFrame
+    df = pd.DataFrame(Patients.return_list_of_dict(patients))
+
+    # Preprocess the data using your scaler
+    scaled_data = scaler.transform(df)
+
+    # Make predictions using your model
+    predictions = model.predict(scaled_data)
+
+    # Return the predictions
+    return {"predictions": predictions.tolist()}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
